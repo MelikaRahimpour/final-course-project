2c5a06a62fe148a5f6f13d352fda8b03b7cd949e
MR.Co <melikarahimpour@gmail.com>
2025-10-01 15:02:07 +0330

    created section component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import {Section} from "@/components";
 
 export default function Home() {
-  return (
-    <> this is Home</>
-  );
+    return(
+        <>
+            <Section className={'mb-[75px]'}>
+
+            </Section>
+        </>
+    )
 }
