65a7e529c0f65efdf28047f14e80eff8468f1cbc
MR.Co <melikarahimpour@gmail.com>
2025-10-01 18:39:00 +0330

    implement banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import {Section} from "@/components";
+import {Banner, Section} from "@/components";
 
 export default function Home() {
     return(
         <>
             <Section className={'mb-[75px]'}>
-
+                <Banner title={'Don’t miss amazing grocery deals'}
+                subtitle={"Sign up for the daily newsletter"}
+                bgImage={"/assets/images/banner_bg.png"}
+                image={'/assets/images/fresh-apples.png'}/>
             </Section>
         </>
     )
