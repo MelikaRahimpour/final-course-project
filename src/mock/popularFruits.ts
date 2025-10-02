8f44191c5cc87c488a29c0973e832b1ca3cdb604
MR.Co <melikarahimpour@gmail.com>
2025-10-02 19:13:34 +0330

    implement product slider but have some issues with product card

---
+++ src/mock/popularFruits.ts
@@ -0,0 +1,68 @@
+export const popularFruits = [
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice' ,
+        image: '/assets/images/products/mango.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 3000,
+        sale_price: 200,
+        label: 'Hot'
+    },
+    {
+        title: 'Blue Diamond Almonds Lightly Salted Vegetables' ,
+        image: '/assets/images/products/lemon.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 3000,
+        sale_price: 200,
+        label: 'Hot'
+    },
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice' ,
+        image: '/assets/images/products/cucumber.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 22000,
+        sale_price: 700,
+        label: '17%'
+    },
+    {
+        title: 'All Natural Italian-Style Chicken Meatballs' ,
+        image: '/assets/images/products/corn.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 3000,
+        sale_price: 500,
+        label: 'Hot'
+    },
+    {
+        title: 'Foster Farms Takeout Crispy Classic Buffalo Wings' ,
+        image: '/assets/images/products/paper.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 5000,
+        sale_price: 200,
+        label: ''
+    },
+    {
+        title: 'Angie’s Boomchickapop Sweet & Salty Kettle Corn' ,
+        image: '/assets/images/products/tanjerin.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 3000,
+        sale_price: 200,
+        label: 'Sale'
+    }
+]
---
+++ src/mock/popularProducts.ts
@@ -0,0 +1,57 @@
+export const popularProducts = [
+    {
+        title: 'All Natural Italian-Style Chicken Meatballs' ,
+        image: '/assets/images/products/corn.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 3500,
+        sale_price: 2000,
+        label: 'Hot'
+    },
+    {
+        title: 'Angie’s Boomchickapop Sweet & Salty Kettle Corn' ,
+        image: '/assets/images/products/tanjerin.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 3000,
+        sale_price: 200,
+        label: 'Sale'
+    },
+    {
+        title: 'Foster Farms Takeout Crispy Classic Buffalo Wings' ,
+        image: '/assets/images/products/paper.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 3000,
+        sale_price: 200,
+        label: ''
+    },
+    {
+        title: 'Blue Diamond Almonds Lightly Salted Vegetables' ,
+        image: '/assets/images/products/lemon.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 3200,
+        sale_price: 200,
+        label: 'Hot'
+    },
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice' ,
+        image: '/assets/images/products/cucumber.png' ,
+        category: 'Hodo Foods' ,
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram' ,
+        price: 4000,
+        sale_price: 200,
+        label: '17%'
+    }
+]
