f56ca7dc3bf1ecda443970f6ea07af5cff12f92b
MR.Co <melikarahimpour@gmail.com>
2025-10-02 11:43:58 +0330

    implement featured categories section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import {Banner, Section} from "@/components";
+import {FeaturedCategories} from "@/components/pages/index";
 
 export default function Home() {
     return(
         <>
-            <Section className={'mb-[75px]'}>
+            <Section>
                 <Banner title={'Don’t miss amazing grocery deals'}
                 subtitle={"Sign up for the daily newsletter"}
                 bgImage={"/assets/images/banner_bg.png"}
                 image={'/assets/images/fresh-apples.png'}/>
+            </Section>
+            <Section className={"mb-[75px]"}>
+                <div className="hidden sm:flex mb-[50px]">
+                    <h2 className="text-heading3 text-blue-300">Featured Categories</h2>
+                </div>
+                <FeaturedCategories />
             </Section>
         </>
     )
