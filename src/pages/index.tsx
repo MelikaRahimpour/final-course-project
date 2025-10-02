d696d8a96cc42a229ab268b3cb9e96d8343ef58b
MR.Co <melikarahimpour@gmail.com>
2025-10-02 19:13:24 +0330

    implement product slider but have some issues with product card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import {Banner, MiniProductSlider, Section} from "@/components";
-import {FeaturedCategories} from "@/components/pages/index";
+import {Banner, FeaturedCategories, IconBox, MiniProductSlider, Section, SimpleProductSlider} from "@/components";
+import {popularProducts} from "@/mock/popularProducts";
+import {popularFruits} from "@/mock/popularFruits";
 
 export default function Home() {
     return(
@@ -18,6 +19,26 @@ export default function Home() {
             </Section>
             <Section>
                 <MiniProductSlider/>
+            </Section>
+            <Section>
+                <div className="flex justify-between mb-[50px]">
+                    <h2 className="text-heading3 text-blue-300">Popular Products</h2>
+                    <div className="flex items-center gap-3">
+                        <IconBox icon={"swiper-nav-left icon-angle-small-left cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white text-[24px]"}></IconBox>
+                        <IconBox icon={"swiper-nav-right icon-angle-small-right cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white text-[24px]"}></IconBox>
+                    </div>
+                </div>
+                <SimpleProductSlider nextEl={".swiper-nav-right"} prevEl={".swiper-nav-left"} sliderData={popularProducts}/>
+            </Section>
+            <Section>
+                <div className="flex justify-between mb-[50px]">
+                    <h2 className="text-heading3 text-blue-300">Popular Fruits</h2>
+                    <div className="flex items-center gap-3">
+                        <IconBox icon={"swiper-nav-left2 icon-angle-small-left cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white text-[24px]"}></IconBox>
+                        <IconBox icon={"swiper-nav-right2 icon-angle-small-right cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white text-[24px]"}></IconBox>
+                    </div>
+                </div>
+                <SimpleProductSlider nextEl={".swiper-nav-right2"} prevEl={".swiper-nav-left2"} sliderData={popularFruits}/>
             </Section>
         </>
     )
